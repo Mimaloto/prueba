71f6208399c70846cd0820a0e052e9dfae821d3a
Mimaloto <140119457+Mimaloto@users.noreply.github.com>
2024-03-20 16:04:23 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+const textArea =document.querySelector(".campoTexto");
+const mensaje =document.querySelector(".mensaje");
+
+//La letra "e" es convertida para "enter"
+//La letra "i" es convertida para "imes"
+//La letra "a" es convertida para "ai"
+//La letra "o" es convertida para "ober"
+//La letra "u" es convertida para "ufat"
+
+function btnEncriptar(){
+    const textoEncriptado = encriptar(textArea.value)
+    mensaje.value = textoEncriptado
+    textArea.value = "";
+    mensaje.stayle.backgroundimage = "none";
+
+}
+
+function encriptar(stringEncriptado){
+    let matrizCodigo = [["e","enter"],["i","imes"],["a","ai"],["o","ober"],["u","ufat"]];
+    stringEncriptado = stringEncriptado.tolowerCase()
+
+    for(let i = 0; i < matrizCodigo.length; i++){
+        if(stringEncriptado.includes(matrizCodigo[i][0])){
+            stringEncriptado = stringEncriptado.replaceall(matrizCodigo[i][0],matrizCodigo[i][1])
+        }
+
+    }
+    return stringEncriptado
+}
+
+function btnDesencriptar(){
+    const textoEncriptado = desencriptar(textArea.value)
+    mensaje.value = textoEncriptado
+    textArea.value = "";
+   
+
+}
+
+function desencriptar(stringDesencriptado){
+    let matrizCodigo = [["e","enter"],["i","imes"],["a","ai"],["o","ober"],["u","ufat"]];
+    stringDesencriptado = stringDesencriptado.tolowerCase()
+
+    for(let i = 0; i < matrizCodigo.length; i++){
+        if(stringDesencriptado.includes(matrizCodigo[i][1])){
+            stringDesencriptado = stringDesencriptado.replaceall(matrizCodigo[i][1],matrizCodigo[i][0])
+        }
+
+    }
+    return stringDesencriptado
+}
